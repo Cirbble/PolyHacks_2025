72c2b7d3129859f2e98a832783449128a0ffa5c4
spilledthoughts <ullah5389@gmail.com>
2025-02-08 22:30:51 -0500

    da

---
+++ light_vibes_project/src/pages/DataPage.tsx
@@ -0,0 +1,12 @@
+const DataPage = () => {
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold mb-4">Marine Species Data</h1>
+      <div className="bg-white rounded-lg shadow p-6">
+        <p>Data visualization and analysis will go here</p>
+      </div>
+    </div>
+  );
+};
+
+export default DataPage; 
